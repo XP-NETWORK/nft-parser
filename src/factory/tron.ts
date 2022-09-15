3d1e2b4c8ad882500bafb144cda556c78be9a60c
AlexeyAdoniev <Alexeya@xp.network>
2022-09-15 12:28:15 +0300

    tron space

--- src/factory/tron.ts
+++ src/factory/tron.ts
@@ -5,7 +5,7 @@ import { nftGeneralParser } from "..";
 import requestPool from "../../tools/requestPool";
 import { setupURI } from ".";
 import { getAssetFormat } from "../../tools/helpers";
-import { proxy } from "..";
+import { proxy, apenftKey, apenftSign } from "..";
 
 const pool = requestPool(3000);
 
@@ -40,11 +40,82 @@ export const tronParser = async (
 ) => {
   let parsed;
   switch (collectionIdent) {
+    case "TGkgcveyPzhzj18dtxpzaAtcL5ZUB8965A": {
+      parsed = await SpaceClub(nft, account, whitelisted);
+      break;
+    }
+
     default:
       parsed = await Default(nft, account, whitelisted);
       break;
   }
   return parsed;
+};
+
+export const SpaceClub = async (
+  nft: any,
+  account: string,
+  whitelisted: boolean
+): Promise<NFT> => {
+  const {
+    native,
+    native: { contract, tokenId, chainId },
+    collectionIdent,
+    uri,
+  } = nft;
+
+  const query = {
+    operationName: "assetsDetail",
+    variables: {
+      blockChain: {
+        chain: "tron",
+        chainId: "0x2b6653dc",
+      },
+      contractAddress: "TGkgcveyPzhzj18dtxpzaAtcL5ZUB8965A",
+      tokenId: String(tokenId),
+    },
+    query:
+      "query assetsDetail($blockChain: BlockChainInput!, $contractAddress: Address!, $tokenId: String!, $thirdStandards: [String!]) {\n  asset(\n    identity: {blockChain: $blockChain, contractAddress: $contractAddress, tokenId: $tokenId}\n  ) {\n    id\n    chain\n    chainId\n    contractAddress\n    tokenId\n    tokenType\n    creatorAddress\n    collectionId\n    name\n    description\n    imageUrl\n    imagePreviewUrl\n    imageThumbnailUrl\n    animationUrl\n    externalUrls\n    unlockContentEnable\n    ownedQuantity\n    ownershipCount\n    quantity\n    properties {\n      key\n      value\n      traitCount\n      traitTotal\n      traitSupply\n      __typename\n    }\n    levels {\n      key\n      value\n      max\n      __typename\n    }\n    stats {\n      key\n      value\n      max\n      __typename\n    }\n    supply\n    tradeSummary(thirdStandards: $thirdStandards) {\n      lastSale {\n        lastSalePrice\n        lastSaleTokenContract {\n          id\n          name\n          address\n          icon\n          decimal\n          symbol\n          accuracy\n          __typename\n        }\n        __typename\n      }\n      bestBid {\n        bestBidPrice\n        bestBidToken\n        bestBidPriceBase\n        bestBidPriceUSD\n        bestBidPriceCNY\n        bestBidCreatedDate\n        bestBidOrderString\n        bestBidOrderQuantity\n        bestBidTokenContract {\n          id\n          name\n          address\n          icon\n          decimal\n          symbol\n          accuracy\n          __typename\n        }\n        __typename\n      }\n      bestAsk {\n        bestAskSaleKind\n        bestAskPrice\n        bestAskToken\n        bestAskPriceBase\n        bestAskPriceUSD\n        bestAskExpirationDate\n        bestAskOrderString\n        bestAskOrderType\n        bestAskOrderQuantity\n        bestAskTokenContract {\n          id\n          name\n          address\n          icon\n          decimal\n          symbol\n          accuracy\n          __typename\n        }\n        __typename\n      }\n      __typename\n    }\n    assetOwners(first: 1) {\n      edges {\n        cursor\n        node {\n          chain\n          chainId\n          owner\n          balance\n          account {\n            identity {\n              address\n              blockChain {\n                chain\n                chainId\n                __typename\n              }\n              __typename\n            }\n            user {\n              id\n              address\n              profileImageUrl\n              userName\n              __typename\n            }\n            info {\n              profileImageUrl\n              userName\n              __typename\n            }\n            __typename\n          }\n          __typename\n        }\n        __typename\n      }\n      __typename\n    }\n    creator {\n      identity {\n        address\n        blockChain {\n          chain\n          chainId\n          __typename\n        }\n        __typename\n      }\n      user {\n        id\n        address\n        profileImageUrl\n        userName\n        __typename\n      }\n      info {\n        profileImageUrl\n        userName\n        __typename\n      }\n      __typename\n    }\n    paymentTokens {\n      id\n      name\n      address\n      icon\n      symbol\n      chain\n      chainId\n      decimal\n      __typename\n    }\n    collection {\n      id\n      name\n      slug\n      description\n      imageUrl\n      bannerImageUrl\n      featuredImageUrl\n      externalUrl\n      weiboUrl\n      twitterUrl\n      instagramUrl\n      mediumUrl\n      telegramUrl\n      discordUrl\n      facebookUrl\n      paymentTokens {\n        id\n        name\n        icon\n        symbol\n        chain\n        chainId\n        decimal\n        address\n        __typename\n      }\n      isVerified\n      royalty\n      royaltyAddress\n      __typename\n    }\n    uri\n    isFavorite\n    favoriteQuantity\n    __typename\n  }\n}\n",
+  };
+
+  try {
+    const res = await axios.post(
+      proxy + "https://api.apenft.io/graphql",
+      query,
+      {
+        headers: {
+          "x-api-key": apenftKey,
+          "x-api-sign": apenftSign,
+        },
+      }
+    );
+
+    const asset = res?.data?.data?.asset;
+
+    const nft: NFT = {
+      native,
+      chainId,
+      tokenId,
+      owner: account,
+      uri,
+      contract,
+      collectionIdent,
+      wrapped: null,
+      metaData: {
+        whitelisted,
+        image: asset?.imageUrl,
+        imageFormat: asset?.imageUrl?.match(/(?:\.([^.]+))?$/)?.at(1),
+        attributes: asset?.properties,
+        description: asset.description,
+        name: asset.name,
+        contractType: asset.tokenType,
+      },
+    };
+    return nft;
+  } catch (error: any) {
+    console.error(error.code || error.status);
+    return nft;
+  }
 };
 
 export const Default = async (
--- src/index.ts
+++ src/index.ts
@@ -25,6 +25,10 @@ export const proxy = isNode
   ? ""
   : "https://sheltered-crag-76748.herokuapp.com/";
 
+export const apenftKey = "rV9UjZwMSK4zqkKEWOUnUXXY2zNgPJ8i";
+export const apenftSign =
+  "7c9caa14981ff714f92fe16322bcf13803cd3c0d219ef008eb0e5ebf352814ca.7625.1663231473";
+
 axios.defaults.timeout = isNode ? 2500 : axios.defaults.timeout;
 axios.interceptors.request.use(
   function (config) {
