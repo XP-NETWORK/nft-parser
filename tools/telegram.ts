d882105502082675b20b0729b31a58b81fe868de
rony derra <derrarony@gmail.com>
2022-10-31 10:25:37 +0200

    a

--- tools/telegram.ts
+++ tools/telegram.ts
@@ -6,7 +6,7 @@ export const sendTelegramMessage = async (msg: any) => {
         console.log("TELEGRAM!!!!!!!");
         console.log({ msg });
 
-        let str = `%0Auri: ${msg.uri} %0A<strong>chainId: ${msg.native.chainId}%0AtokenId ${msg.native.tokenId}%0Acontract ${msg.native.contract}%owner ${msg.native.owner}%0AcontractType ${msg.native.contractType}%0AcollName${msg.native.name}</strong>`
+        let str = `%0Auri: ${msg.uri} %0A<strong>chainId: ${msg.native.chainId}%0AtokenId ${msg.native.tokenId}%0Acontract ${msg.native.contract}%0Aowner ${msg.native.owner}%0AcontractType ${msg.native.contractType}%0AcollName${msg.native.name}</strong>`
         await axios.get(`https://api.telegram.org/bot5649872974:AAHye96JTkXxrMfkg8FnM4hI30Z4q3xAt00/sendMessage?chat_id=-850284174&text=${str}&parse_mode=HTML`);
     } catch (err: any) {
         console.log(err)
