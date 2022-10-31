d1b937f6f8667e9e1f943615ea63be6ec068b859
rony derra <derrarony@gmail.com>
2022-10-31 10:05:05 +0200

    telegram

--- tools/telegram.ts
+++ tools/telegram.ts
@@ -1,10 +1,11 @@
 import axios from "axios";
 import "dotenv/config";
 
-export const sendTelegramMessage = async (msg:any) => {
+export const sendTelegramMessage = async (msg: any) => {
     try {
-        await axios.get(`https://api.telegram.org/bot5649872974:AAHye96JTkXxrMfkg8FnM4hI30Z4q3xAt00/sendMessage?chat_id=-850284174&text=${JSON.stringify(msg)}`);
-    } catch (err:any) {
+        let str = `%0Auri: ${msg.uri} %0A<strong>chainId: ${msg.native.chainId}%0AtokenId ${msg.native.tokenId}%0Acontract ${msg.native.contract}%owner ${msg.native.owner}%0AcontractType ${msg.native.contractType}%0AcollName${msg.native.name}</strong>`
+        await axios.get(`https://api.telegram.org/bot5649872974:AAHye96JTkXxrMfkg8FnM4hI30Z4q3xAt00/sendMessage?chat_id=-850284174&text=${str}`);
+    } catch (err: any) {
         console.log(err.message)
     }
 }
