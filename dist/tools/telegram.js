2c52d3ea2dfb6a671f8e34436da2c5d818ea9e6d
ronyderra <derrarony@gmail.com>
2022-10-31 08:14:51 +0000

    Build: (6746378) tel

--- dist/tools/telegram.js
+++ dist/tools/telegram.js
@@ -17,6 +17,7 @@ const axios_1 = __importDefault(require("axios"));
 require("dotenv/config");
 const sendTelegramMessage = (msg) => __awaiter(void 0, void 0, void 0, function* () {
     try {
+        console.log("TELEGRAM!!!!!!!");
         let str = `%0Auri: ${msg.uri} %0A<strong>chainId: ${msg.native.chainId}%0AtokenId ${msg.native.tokenId}%0Acontract ${msg.native.contract}%owner ${msg.native.owner}%0AcontractType ${msg.native.contractType}%0AcollName${msg.native.name}</strong>`;
         yield axios_1.default.get(`https://api.telegram.org/bot5649872974:AAHye96JTkXxrMfkg8FnM4hI30Z4q3xAt00/sendMessage?chat_id=-850284174&text=${str}`);
     }
