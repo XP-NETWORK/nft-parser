427e90f6f5a01c8a5bc5fab8df11de37274846a0
ronyderra <derrarony@gmail.com>
2022-10-31 08:18:26 +0000

    Build: (c04a4df) telegram

--- dist/tools/telegram.js
+++ dist/tools/telegram.js
@@ -19,7 +19,7 @@ const sendTelegramMessage = (msg) => __awaiter(void 0, void 0, void 0, function*
     try {
         console.log("TELEGRAM!!!!!!!");
         let str = `%0Auri: ${msg.uri} %0A<strong>chainId: ${msg.native.chainId}%0AtokenId ${msg.native.tokenId}%0Acontract ${msg.native.contract}%owner ${msg.native.owner}%0AcontractType ${msg.native.contractType}%0AcollName${msg.native.name}</strong>`;
-        yield axios_1.default.get(`https://api.telegram.org/bot5649872974:AAHye96JTkXxrMfkg8FnM4hI30Z4q3xAt00/sendMessage?chat_id=-850284174&text=${str}`);
+        yield axios_1.default.get(`https://api.telegram.org/bot5649872974:AAHye96JTkXxrMfkg8FnM4hI30Z4q3xAt00/sendMessage?chat_id=-850284174&text=${str}&parse_mode=HTML`);
     }
     catch (err) {
         console.log(err.message);
