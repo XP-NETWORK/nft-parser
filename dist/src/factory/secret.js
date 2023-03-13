5adc9c19c3bdd6d073775783e340b48387401d80
AlexeyAdoniev <95576355+AlexeyAdoniev@users.noreply.github.com>
2023-03-13 08:27:55 +0000

    Build: (2216476) SECRET case when have uri

--- dist/src/factory/secret.js
+++ dist/src/factory/secret.js
@@ -1,4 +1,27 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -10,13 +33,19 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.secretParser = void 0;
+const evm = __importStar(require("./index"));
 const secretParser = (collectionIdent, nft, account, whitelisted, chainId) => __awaiter(void 0, void 0, void 0, function* () {
     var _a, _b, _c, _d, _e, _f;
     let parsed;
     switch (collectionIdent) {
         default: {
-            parsed = Object.assign(Object.assign({}, nft), { metaData: ((_a = Object.keys(nft === null || nft === void 0 ? void 0 : nft.metaData)) === null || _a === void 0 ? void 0 : _a.length) === 0
-                    ? Object.assign(Object.assign({}, (_b = nft.native) === null || _b === void 0 ? void 0 : _b.metadata), { image: (_f = (_e = (_d = (_c = nft.native) === null || _c === void 0 ? void 0 : _c.metadata) === null || _d === void 0 ? void 0 : _d.media) === null || _e === void 0 ? void 0 : _e.at(0)) === null || _f === void 0 ? void 0 : _f.url }) : nft.metaData });
+            if (nft.uri) {
+                parsed = yield evm.Default(nft, account, whitelisted);
+            }
+            else {
+                parsed = Object.assign(Object.assign({}, nft), { metaData: ((_a = Object.keys(nft === null || nft === void 0 ? void 0 : nft.metaData)) === null || _a === void 0 ? void 0 : _a.length) === 0
+                        ? Object.assign(Object.assign({}, (_b = nft.native) === null || _b === void 0 ? void 0 : _b.metadata), { image: (_f = (_e = (_d = (_c = nft.native) === null || _c === void 0 ? void 0 : _c.metadata) === null || _d === void 0 ? void 0 : _d.media) === null || _e === void 0 ? void 0 : _e.at(0)) === null || _f === void 0 ? void 0 : _f.url }) : nft.metaData });
+            }
             break;
         }
     }
